84fa454b0dc9a8b446cb85a9b1c28d7752855bde
linker03 <linker03@gmail.com>
2021-11-17 12:20:13 +0600

    add header icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,6 @@
 export { default as Youtube } from './youtube';
 export { default as Instagram } from './instagram';
 export { default as Facebook } from './facebook';
+export { default as Chat } from './chat';
+export { default as Localization } from './localization';
+export { default as Search } from './search';
