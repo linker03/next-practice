54baff674f7e9a21a7c8648f41ac3c933bbad8e3
linker03 <linker03@gmail.com>
2021-11-18 16:09:57 +0600

    add new icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -8,3 +8,5 @@ export { default as BurgerIcon } from './burger-icon';
 export { default as HomeIcon } from './home-icon';
 export { default as CloseIcon } from './close-icon';
 export { default as ArrowRight } from './arrow-right';
+export { default as DropdownArrow } from './dropdown-arrow';
+export { default as CheckIcon } from './check-icon';
