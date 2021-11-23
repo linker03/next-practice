37d164b2d8393ca9d88c3348a59689c0ce6760e9
linker03 <success@extramilelabs.com>
2021-11-23 16:20:50 +0600

    add page top hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { useOnClickOutside } from './use-click-outside';
 export { useWindowScroll } from './use-window-scroll';
 export { useWhiteMenu } from './use-white-menu';
+export { usePageTop } from './use-page-top';
